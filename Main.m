11c3920279cc65e0fe84d4d6ea3461284de8989f
fxzhong <billyzhonghk@gmail.com>
2019-12-12 17:10:02 +0800

    v1.0.1 Add parameter display and a decent demo

--- Main.m
+++ Main.m
@@ -18,11 +18,11 @@ writerObj.FrameRate = 30; % How many frames per second.
 open(writerObj);
 
 %conrol time horizon
-t_h = 30;
+t_h = 100;
 %control task specification
 x_obst = [0.3*width, 0.5*height-50, 80]; %the size is in radius
 x_start = [50, 0.5*height];
-x_end = [0.5*width, 0.4*height];
+x_end = [0.6*width, 0.4*height];
 
 x0 = x_start;
 x_current = x0;
@@ -37,13 +37,18 @@ u0(:,2) = 2;
 u = u0;
 
 t = 0;
+t_step = 0;
 obj = 0;
 obj_old = 0;
 d_obj = 1000;
+%preset parameters for online tuning
+du_k = 1;
+d_obj_th = 1;
+weight = 0.1;
 
 while true
 
-    while abs(d_obj) > 5
+    while abs(d_obj) > d_obj_th
         %Calculate the initial estimated states evolution from t1 to tn
         x(1,:) = x_current;
         for i = 1:t_h-1
@@ -53,13 +58,13 @@ while true
         %Caculate costate Lambda from Lagrangian Multiplier from tn back to t1
         phi = 0.5*norm(x(t_h,:) - x_end)^2;
         lambda = zeros(t_h, 2);
-        lambda(t_h, :) = -0.002*(x(t_h,:) - x_end);
+        lambda(t_h, :) = -0.001*(x(t_h,:) - x_end);
         for i = t_h-1:-1:1
             %lambda(i) = lambda(i+1) + dH/dx
-            lambda(i, :) = lambda(i+1, :) + 0.5*(x(i,:)-x_obst(1:2))/(norm(x(i,:)-x_obst(1:2))- x_obst(3))/(norm(x(i,:)-x_obst(1:2)) - x_obst(3))/norm(x(i,:)-x_obst(1:2));
+            lambda(i, :) = lambda(i+1, :) + weight*(x(i,:)-x_obst(1:2))/(norm(x(i,:)-x_obst(1:2))- x_obst(3))/(norm(x(i,:)-x_obst(1:2)) - x_obst(3))/norm(x(i,:)-x_obst(1:2));
         end
 
-        u = u + lambda;
+        u = u + du_k*lambda;
     
         %Calculate objective function with the control horizon
         obj = phi;
@@ -68,12 +73,13 @@ while true
         end
         d_obj = obj - obj_old;
         obj_old = obj;
+        t_step = t_step + 1;
     end
     
     %Plot trivial things
     plot(x_obst(1), x_obst(2), '.k', 'MarkerSize', 4*x_obst(3));hold on;
-    plot(x_start(1), x_start(2), 'ok', 'MarkerSize',10,'MarkerFaceColor','g');
-    plot(x_end(1), x_end(2), 'ok', 'MarkerSize',10,'MarkerFaceColor','r');
+    plot(x_start(1), x_start(2), 'ok', 'MarkerSize',5,'MarkerFaceColor','g');
+    plot(x_end(1), x_end(2), 'ok', 'MarkerSize',5,'MarkerFaceColor','r');
     plot([x_traj_start(:,1), x_traj_end(:,1)], [x_traj_start(:,2), x_traj_end(:,2)],'-','Color','r','LineWidth',3);      
     %Plot variables
     plot(x_current(1), x_current(2), '.r', 'MarkerSize', 5);
@@ -82,6 +88,14 @@ while true
     text(30,30,textt,'Color','k');
     textt = ['dObj: ', num2str(d_obj)];
     text(30,60,textt,'Color','k');
+    textt = ['Opti_t: ', num2str(t_step)];
+    text(30,90,textt,'Color','k');
+    textt = ['du-k: ', num2str(du_k)];
+    text(30,height-30,textt,'Color','k');
+    textt = ['dObj-th: ', num2str(d_obj_th)];
+    text(30,height-60,textt,'Color','k');
+    textt = ['weight: ', num2str(weight)];
+    text(30,height-90,textt,'Color','k');
     %Keyboard input callback
     if strcmpi(get(billy,'CurrentKey'),'q')
         close(writerObj);
@@ -106,7 +120,9 @@ while true
     writeVideo(writerObj, frame);
     
     t = t + 1;
-
+    
+    t_step = 0;
+    %only apply the first controller input to the final execution 
     x_current = x_current + u(1,:);
     
     %delete the endpoint flag
